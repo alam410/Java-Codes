569e6ccdd77a9ea6bcbf0e2144668aa176d7a8c9
Md. Thaiabul Alam Chowdhury <123104873+alam410@users.noreply.github.com>
2024-09-24 23:16:00 +0600

    Add files via upload

---
+++ Backtracking/src/Backtracking_Arrays.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Backtracking_Arrays {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the length of array:");int n = sc.nextInt();
+        int arr[]=new int[n];
+        System.out.println("Enter the elements:");
+        for(int i =0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.print("Enter the value: ");
+        int val = sc.nextInt();
+        changeArr(arr,0,val);
+        printArr(arr);
+
+    }
+    public static void changeArr(int arr[],int i,int val){
+        if(i== arr.length){
+        printArr(arr);
+        return;
+        }
+
+        arr[i]=val;
+        changeArr(arr, i+1, val+2);
+        arr[i]=arr[i]-2;
+    }
+    public static void printArr(int arr[]){
+        for(int i =0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
